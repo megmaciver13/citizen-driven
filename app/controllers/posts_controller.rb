4ef6c2cc2284729990ade5b0d28d5d5a383da9b0
Meg MacIver <megmaciver13@gmail.com>
2017-11-28 15:36:50 -0500

    create edit and new controller methods and views for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,39 @@
 class PostsController < ApplicationController
+  def new
+    @landmark = Landmark.find(params[:landmark_id])
+    @post = @landmark.posts.new
+  end
+
+  def create
+    @landmark = Landmark.find(params[:landmark_id])
+    @landmark.posts.create(post_params)
+    redirect_to landmark_path(@landmark)
+  end
+
   def show
     @landmark = Landmark.find(params[:landmark_id])
     @post = @landmark.posts.find(params[:id])
+  end
+
+  def edit
+    @landmark = Landmark.find(params[:landmark_id])
+    @post = @landmark.posts.find(params[:id])
+  end
+
+  def update
+    @landmark = Landmark.find(params[:landmark_id])
+    @post = @landmark.posts.find(params[:id])
+    @post.update(post_params)
+  end
+
+  def destroy
+    @landmark = Landmark.find(params[:landmark_id])
+    @post = @landmark.posts.find(params[:id])
+    @post.destroy
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:author, :title, :photo_url, :content, :inquiry_type)
   end
 end
