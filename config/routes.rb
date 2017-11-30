35630b7994ba80c47a3f5d6bb314c4706bbcc336
Meg MacIver <megmaciver13@gmail.com>
2017-11-30 08:38:10 -0500

    clean up CSS

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "neighborhoods#index"
+  root to: "pages#show", page: "about"
   resources :neighborhoods do
     resources :posts
   end
