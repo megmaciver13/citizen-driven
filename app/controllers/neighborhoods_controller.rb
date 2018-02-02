babee4ee792e27564e80d6fae959be86df17770e
Meg MacIver <megmaciver13@gmail.com>
2018-02-02 09:18:53 -0500

    update neighborhood show page; post show page

--- app/controllers/neighborhoods_controller.rb
+++ app/controllers/neighborhoods_controller.rb
@@ -5,10 +5,5 @@ class NeighborhoodsController < ApplicationController
 
   def show
     @neighborhood = Neighborhood.find(params[:id])
-    # Gmaps4rails.build_markers(@neighborhood.posts) do |post, marker|
-    #   marker.lat post.latitude
-    #   marker.lng post.longitude
-    #   marker.infowindow post.title
-    # end
   end
 end
